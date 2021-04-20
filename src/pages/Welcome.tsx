d7f0beb5b118ebc56e3cf9752b34a3fb2a49828d
josealves380 <netopereira464@gmail.com>
2021-04-20 14:56:46 -0300

    Criando novas páginas,inserindo icons definido a navegação entre as páginas,definindo as fonts

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,35 +1,59 @@
-import React, { useState} from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import { 
+    SafeAreaView,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-import wateringImg from '../assets/watering.png'
-import { Button } from '../components/Button';
+import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-
-export function Welcome(){
-    const [visible, setVisible] = useState(false);
-
-    function handleVisibility(){
-        setVisible(true)
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
+export function Welcome(){  
+    const navigation = useNavigation();
+    
+    function handleStart(){
+        navigation.navigate('UserIdentification')
     }
+
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie{'\n'} 
-                suas plantas{'\n'} 
-                de forma fácil
-            </Text>
+           <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie{'\n'} 
+                    suas plantas de{'\n'} 
+                    forma fácil
+                </Text>
 
-            {
-                visible &&
-                <Image source={wateringImg} style={styles.image} />
-            }
+                <Image 
+                    source={wateringImg}    
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+            
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas.
+                    Nós cuidamos de lembrar você sempre que prescisar.
+                </Text>
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas.
-                Nós cuidamos de lembrar você sempre que prescisar.
-            </Text>
-
-            <Button title=">" onPress={handleVisibility}/>
+                <TouchableOpacity 
+                    style={styles.button} 
+                    activeOpacity={0.7}
+                    onPress={handleStart}
+                >
+                    
+                    <Feather 
+                        name="chevron-right"
+                        style={styles.buttonIcon} 
+                    />
+                    
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -37,21 +61,31 @@ export function Welcome(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal:20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    image: {
+        height: Dimensions.get('window').width * 0.7
     },
     button: {
         backgroundColor: colors.green,
@@ -60,14 +94,10 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         marginBottom: 10,
         height: 56,
-        width: 56
+        width: 56,
     },
-    image: {
-        width:292,
-        height: 284
-    },
-    buttonText:{
+    buttonIcon:{
         color: colors.white,
-        fontSize: 24
+        fontSize: 32
     }
 })
