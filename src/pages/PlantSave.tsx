7a6bba095dd1bf90baa728dac77cd68990e06467
josealves380 <netopereira464@gmail.com>
2021-04-22 22:53:38 -0300

    Criando página de salvar plantas no dispositivo do usuário aproveitando páginas com a mesma configurações

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -11,12 +11,16 @@ import {
 } from 'react-native';
 import { SvgFromUri } from 'react-native-svg';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
-import {useRoute} from '@react-navigation/core';
+
+import { Button } from '../components/Button';
+
+
+import {useNavigation, useRoute} from '@react-navigation/core';
 import DateTimerPicker, { Event } from '@react-native-community/datetimepicker';
 import { format, isBefore } from 'date-fns';
 import { PlantProps, savePlant } from '../libs/storage';
 
-import { Button } from '../components/Button';
+
 
 import waterdrop from '../assets/waterdrop.png';
 import colors from '../styles/colors';
@@ -33,6 +37,8 @@ export function PlantSave() {
 
     const route = useRoute();
     const { plant } = route.params as Params;
+
+    const navigation = useNavigation();
 
     function handleChangeTime(event: Event, dateTime: Date | undefined){
         if(Platform.OS === 'android'){
@@ -58,6 +64,15 @@ export function PlantSave() {
                 ...plant,
                 dateTimeNotification: selectedDateTime
             });
+
+            navigation.navigate('Confirmation', {
+                title: 'Tudo certo',
+                subtitle: 'Fique tranquilo que sempre vamos lembrar você de cuidar da sua plantinha com muito cuidado.',
+                buttonTitle: 'Muito Obrigado :D',
+                icon: 'hug',
+                nextScreen: 'MyPlants',
+            });
+
         }catch {
             Alert.alert('Não foi possível salva. 😢');
         }
